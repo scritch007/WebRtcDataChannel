8ef7f4da4c853b41fc996057513bc0e20d21d584
Benjamin Legrand <android@legrand.ws>
2016-02-13 20:36:50 +0100

    First commit. For now we need to enter commands in the console

---
+++ html/js/datachannel_test.js
@@ -0,0 +1,83 @@
+
+// Initialise DataChannel.js
+var datachannel = new DataChannel();
+
+
+datachannel.userid = userid;
+var ws;
+ws = new WebSocket(wspath);
+ws.onopen = function(evt) {
+  console.log("OPEN");
+}
+ws.onclose = function(evt) {
+  console.log("CLOSE");
+  ws = null;
+}
+
+ws.onerror = function(evt) {
+  console.log("ERROR: " + evt.data);
+}
+
+ws.onmessage = function(evt) {
+    var jsonObject = JSON.parse(evt.data);
+    console.log(evt.data);
+    //config.onmessage(jsonObject);
+  }
+
+datachannel.openSignalingChannel = function(config) {
+  var channel = config.channel || this.channel || "default-channel";
+  var xhrErrorCount = 0;
+
+  var socket = {
+    send: function(message) {
+      ws.send(JSON.stringify(message));
+    },
+    channel: channel
+  };
+  ws.onmessage = function(evt) {
+    var jsonObject = JSON.parse(evt.data);
+    console.log(evt.data);
+    config.onmessage(jsonObject);
+    //Now join this one
+  }
+  if (config.onopen) {
+    setTimeout(config.onopen, 1);
+  }
+  return socket;
+}
+
+
+var onCreateChannel = function() {
+  var channelName = cleanChannelName(channelInput.value);
+
+  if (!channelName) {
+    console.log("No channel name given");
+    return;
+  }
+
+  disableConnectInput();
+
+  datachannel.open(channelName);
+};
+
+var onJoinChannel = function() {
+  var channelName = cleanChannelName(channelInput.value);
+
+  if (!channelName) {
+    console.log("No channel name given");
+    return;
+  }
+
+  disableConnectInput();
+
+  // Search for existing data channels
+  datachannel.connect(channelName);
+};
+
+datachannel.onopen = function (userId) {
+  console.log("Yes got it");
+};
+
+datachannel.onmessage = function (message, userId) {
+  console.log("New message");
+};
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	_ "image/png"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+
+	_ "github.com/boombuler/barcode"
+	_ "github.com/boombuler/barcode/qr"
+
+	"github.com/jmcvetta/randutil"
+
+	_ "github.com/scritch007/Racer/types"
+	"github.com/scritch007/go-tools"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+var debug = flag.Bool("debug", false, "Turn into debug mode")
+
+var serverConnections map[string]*websocket.Conn
+
+func init() {
+	serverConnections = make(map[string]*websocket.Conn)
+}
+
+var upgrader = websocket.Upgrader{} // use default options
+func serverWS(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		tools.LOG_ERROR.Print("upgrade:", err)
+		return
+	}
+
+	// Notify the others that we have a new connection pending
+	for _, conn := range serverConnections {
+		tools.LOG_DEBUG.Println("New message to send")
+		conn.WriteMessage(1, []byte(`{"id":"`+id+`"}`))
+	}
+
+	tools.LOG_DEBUG.Println("Entering")
+
+	serverConnections[id] = c
+
+	defer func() {
+		c.Close()
+		delete(serverConnections, id)
+	}()
+
+	for {
+		mt, m, err := c.ReadMessage()
+		if err != nil {
+			tools.LOG_ERROR.Println("read:", err)
+			break
+		}
+		//Forward to everybody except us
+		for cId, conn := range serverConnections {
+			if cId == id {
+				continue
+			}
+			conn.WriteMessage(mt, m)
+		}
+
+	}
+}
+func serveFile(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	filePath := vars["file"]
+	http.ServeFile(w, r, "./html/"+filePath)
+}
+func home(w http.ResponseWriter, r *http.Request) {
+	var id string
+
+	//Create unique RandomUser
+	id, _ = randutil.AlphaString(20)
+
+	tmpl, err := template.ParseFiles("./html/index.html")
+	if err != nil {
+		panic(err)
+	}
+	var socketProto = "ws"
+	if strings.Contains(r.Proto, "HTTPS") {
+		socketProto = "wss"
+	}
+	values := struct {
+		WSocketURL string
+		Id         string
+	}{
+		WSocketURL: socketProto + "://" + r.Host + "/ws/" + id,
+		Id:         id,
+	}
+	tmpl.Execute(w, values)
+}
+
+func main() {
+	tools.LogInit(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
+
+	flag.Parse()
+	tools.LOG_ERROR.SetFlags(0)
+	r := mux.NewRouter()
+	r.HandleFunc("/static/{file:.*}", serveFile)
+	r.HandleFunc("/ws/{id}", serverWS)
+	r.HandleFunc("/", home)
+	http.Handle("/", r)
+	tools.LOG_ERROR.Fatal(http.ListenAndServe(*addr, nil))
+}
